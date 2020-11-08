4369c468ba95720a4bb4e739221ba79c36202b8c
ZoroGH <1223857560@qq.com>
2020-11-08 21:02:08 +0800

    update 2020/11/8
    1. 大致实现了对话系统。
    2. 脚本格式LF与CRLF仍需商讨
    3. 剧本加载方式有待商讨。
    4. 初次实现了用Prefab来控制对话Canvas的想法

---
+++ Assets/Scripts/NPC/DialogScriptHelper.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogScriptHelper
+{
+    /// <summary>
+    /// 剧本
+    /// </summary>
+    private TextAsset script;
+    /// <summary>
+    /// 剧本名
+    /// </summary>
+    public string scriptName;
+    /// <summary>
+    /// 台词列表
+    /// </summary>
+    private List<string> textList;
+    private int curIndex;
+
+
+    /// <summary>
+    /// 创建一个剧本帮助器
+    /// </summary>
+    /// <param name="_script">剧本.txt文件</param>
+    /// <param name="_character">剧本名称</param>
+    public DialogScriptHelper(TextAsset _script, string _character)
+    {
+        if (this.script == null)
+        {
+            this.script = _script;
+            this.scriptName = _character;
+            curIndex = 0;
+            var temp = script.text.Split('\n');
+            textList = new List<string>();
+            foreach (var line in temp)
+            {
+                textList.Add(line);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 读取一行台词,读到文档尾部继续读取会报错
+    /// </summary>
+    /// <returns></returns>
+    public string ReadLine()
+    {
+        if (curIndex != textList.Count)
+            return textList[curIndex++];
+        else
+            throw new System.Exception("已经读到文档结尾");
+    }
+
+
+    /// <summary>
+    /// 测试函数
+    /// </summary>
+    public void PrintScripts()
+    {
+        int idx = 1;
+        foreach (var item in textList)
+        {
+            Debug.Log("# "+(idx++).ToString()+" : "+item);
+        }
+    }
+}
--- Assets/Scripts/NPC/NPCManager.cs
+++ Assets/Scripts/NPC/NPCManager.cs
@@ -1,18 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("UI")]
+    [SerializeField] private GameObject Dialog_Text_FrameWork;
+    [SerializeField] private Text Dialog_Text;
+
+
+    /// <summary>
+    /// 剧本列表
+    /// </summary>
+    [SerializeField] private List<TextAsset> dialogScriptsList;
+    public string curScriptName;
+    public int index_Of_Scripts;//start from 0
+
+    private DialogScriptHelper helper;
+
+    //test
+    public TextAsset test;
+    public DialogScriptHelper dialog_Test;
+
+    
+    private void Start()
     {
-        
+        SetUp();
+
+
+        //test
+        dialog_Test = new DialogScriptHelper(test, "Test");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //按下P键显示对话
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            print(curScriptName);
+
+            ShowText();
+           /* //test
+            string tmp = dialog_Test.ReadLine();
+            print(tmp);//catch a error and destory the class;*/
+        }
     }
+
+    private void SetUp()
+    {
+
+        //获取text文本的Text组件
+        GameObject prefab = Instantiate(Dialog_Text_FrameWork);
+        Dialog_Text = prefab.transform.Find("Text").GetComponent<Text>();
+        dialogScriptsList = new List<TextAsset>();
+        dialogScriptsList.Add(test);
+        //
+        index_Of_Scripts = 0;
+        curScriptName = dialogScriptsList[0].name;
+        //创建一个帮助器
+        helper = new DialogScriptHelper(dialogScriptsList[index_Of_Scripts], curScriptName);
+        
+
+
+    }
+
+    private void ShowText()
+    {
+        this.Dialog_Text.text = helper.ReadLine();
+    }
+
 }
+
+
