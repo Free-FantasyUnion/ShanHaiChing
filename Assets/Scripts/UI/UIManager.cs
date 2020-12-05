a5e3d866fdd619a3e63b0baf236d9de0df7d3eb3
lire <hlq153@163.com>
2020-12-06 01:11:27 +0800

    update 12.6 加了剑影拖尾
    还需要调参...·

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public void gotoScene(string name) {
+        SceneManager.LoadSceneAsync(name);
+    }
     // Start is called before the first frame update
     void Start()
     {
