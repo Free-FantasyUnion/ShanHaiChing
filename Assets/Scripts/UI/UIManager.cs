4c8bce13f42494c86122078ea254fb2330b7ab31
lire <hlq153@163.com>
2020-12-06 16:17:20 +0800

    update 12.6 界面UI更新
    导入了Player的UI
    彩色的buff没有透明度，需要重新加
    将UIManager设置为了单例
    可以通过Set函数的比例来更改遮罩
    遮罩需要有透明度的图来完善

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,22 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    public void gotoScene(string name) {
-        SceneManager.LoadSceneAsync(name);
-    }
-    // Start is called before the first frame update
+    public static UIManager Instance;
+
+    public List<Image> BuffImages;
+
     void Start()
     {
-        
+        Instance = this;
+        foreach (var img in BuffImages)
+        {
+            img.GetComponent<Image>().fillAmount = 1.0f;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setUIRatio(Buff.BuffType buff, float amount)
     {
-        
+        switch(buff)
+        {
+            case Buff.BuffType.AtkUp:
+                BuffImages[0].GetComponent<Image>().fillAmount = amount;
+                break;
+            case Buff.BuffType.SpeedDown:
+                BuffImages[1].GetComponent<Image>().fillAmount = amount;
+                break;
+            case Buff.BuffType.SpeedUp:
+                BuffImages[2].GetComponent<Image>().fillAmount = amount;
+                break;
+            case Buff.BuffType.YuanqiDropSlower:
+                BuffImages[3].GetComponent<Image>().fillAmount = amount;
+                break;
+        }
     }
+
+    
 }
