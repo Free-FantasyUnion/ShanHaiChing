8d61888318ca01d8c53897cc09233670fa1beaf7
ZoroGH <1223857560@qq.com>
2020-12-13 23:10:11 +0800

    开始界面

---
+++ Assets/Scripts/UI/开始界面.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 开始界面 : MonoBehaviour
+{
+    public GameObject 制作人员;
+    public GameObject 游戏说明;
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void 打开制作人员()
+    {
+        制作人员.SetActive(true);
+    }
+    public void 关闭制作人员()
+    {
+        制作人员.SetActive(false);
+    }
+    public void 打开游戏说明()
+    {
+        游戏说明.SetActive(true);
+    }
+    public void 关闭游戏说明()
+    {
+        游戏说明.SetActive(false);
+    }
+
+}
